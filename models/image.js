76fd47912c28095075e3fcd9c520796e9ab77456
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-09-12 05:48:51 -0700

    Create image.js

---
+++ models/image.js
@@ -0,0 +1,7 @@
+module.exports = (sequelize, DataTypes) => {
+    const Image = sequelize.define("images", {
+        title: DataTypes.STRING,
+        url: DataTypes.STRING
+    });
+    return Image;
+}
